55f848828325b48b9dba5ef305172bae938867b3
Dirk Weber <dirk.weber1@googlemail.com>
2016-01-28 23:37:06 +0100

    Added MIT license header to all source files. Using license-plugin for this task

--- core/src/de/dkweb/crillionic/screens/MenuScreen.java
+++ core/src/de/dkweb/crillionic/screens/MenuScreen.java
@@ -1,13 +1,21 @@
+/**
+ * The MIT License (MIT)
+ * Copyright (c) 2016 Dirk Weber
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 package de.dkweb.crillionic.screens;
 
-import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
@@ -15,15 +23,9 @@ import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.utils.I18NBundle;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import de.dkweb.crillionic.Crillionic;
-import de.dkweb.crillionic.model.Highscore;
 import de.dkweb.crillionic.utils.Assets;
 import de.dkweb.crillionic.utils.GraphicUtils;
-import de.dkweb.crillionic.utils.HighscoreManager;
-import de.dkweb.crillionic.utils.JsonManager;
 
-/**
- * Created by dirkweber
- */
 public class MenuScreen implements Screen {
     private Skin skin;
     private Stage stage;
